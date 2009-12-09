504f0dd219c182d2fe834aa3dfbe46008fc26633
dosipov <dosipov@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-12-09 19:56:55 +0000

    * Updated ext_emconf.php after upload
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ttnewscache_cleartag/tags/0_0_1@27530 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "ttnewscache_cleartag"
+# Extension Manager/Repository config file for ext "ttnewscache_cleartag".
 #
-# Auto generated 13-05-2009 11:17
+# Auto generated 09-12-2009 19:51
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.0',
+	'version' => '0.0.1',
 	'constraints' => array(
 		'depends' => array(
 			'tt_news' => '',
@@ -40,7 +40,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"b658";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"df4c";s:20:"doc/wizard_form.html";s:4:"a031";}',
+	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"6059";s:10:"README.txt";s:4:"ee2d";s:32:"class.tx_ttnewscachecleartag.php";s:4:"b4c8";s:36:"class.tx_ttnewscachecleartag_tce.php";s:4:"f3ed";s:16:"ext_autoload.php";s:4:"d797";s:12:"ext_icon.gif";s:4:"ba74";s:17:"ext_localconf.php";s:4:"ce1b";s:14:"doc/manual.sxw";s:4:"34a6";s:19:"doc/wizard_form.dat";s:4:"df4c";s:20:"doc/wizard_form.html";s:4:"a031";}',
+	'suggests' => array(
+	),
 );
 
 ?>
